c238df5c692545bafcc71848f0cb52bab1f43a98
pirozhok228 <vadimpirogov505@gmail.com>
2024-05-01 10:32:23 +0300

    Проект с админкой

---
+++ adminka/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games');
+const bodyParser = require('body-parser');
+const getAllGames = require('./middlewares/games')
+const cors = require('./middlewares/cors');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors,
+    getAllGames,
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
